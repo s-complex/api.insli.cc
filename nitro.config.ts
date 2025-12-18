24b31f6f14b9fedf72123d14658eb026804303d7
gxres042 <i@gxres.net>
2025-12-19 02:08:08 +0800

    fix: use proxy route rule for linklist images

--- nitro.config.ts
+++ nitro.config.ts
@@ -20,5 +20,6 @@ export default defineNitroConfig({
 		'/favicon.ico': {
 			redirect: { to: 'https://library.gxres.net/images/icons/favicon.ico', status: 302 },
 		},
+		'/linklist/img/**': { proxy: 'https://raw.githubusercontent.com/s-complex/Friends/refs/heads/main/img/**', cache: { maxAge: 1209600 } }
 	},
 });
